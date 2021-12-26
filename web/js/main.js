ff70acb47ed6e413c1209a1c9bf17c83b136af36
SKR301 <chunnu.bihat@gmail.com>
2021-12-26 23:58:54 +0530

    added starting point function #5

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,8 @@
 const rows = 5;
 const cols = 5;
 
-var startSet = false;
+var startPos = {'x': -1, 'y': -1};
+var endPos = {'x': -1, 'y': -1};
 
 const maze = Array.from({ length: rows }, () => 
     Array.from({ length: cols }, () => 0)
@@ -36,11 +37,20 @@ function setStart(cell){
     var row = cell.id[5];
     var col = cell.id[7];
 
-    if(maze[row][col] == -1){
-        alert('This is a wall, can\'t be a starting point')
+    if(row == startPos.x && col == startPos.y){
+        maze[row][col] = 0;
+        startPos = {'x':-1, 'y':-1};
+        cell.style.backgroundColor = 'grey';
     } else {
-        maze[row][col] = 1;
-        cell.style.backgroundColor = 'yellow';
+        if(startPos.x == -1 && startPos.y == -1){
+            if(maze[row][col] == -1){
+                alert('This is a wall, can\'t be a starting point')
+            } else {
+                maze[row][col] = 1;
+                startPos = {'x':row, 'y':col};
+                cell.style.backgroundColor = 'yellow';
+            }
+        }
     }
 }
 
