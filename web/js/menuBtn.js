225fc93319bf1f4963fda5f1df457ec81adbec73
SKR301 <chunnu.bihat@gmail.com>
2021-12-30 21:55:57 +0530

    finished up animation #5

--- web/js/menuBtn.js
+++ web/js/menuBtn.js
@@ -1,13 +1,13 @@
 function enableWall(){
     isWall = !isWall;
     if(isWall){
-        document.getElementById('wallBtn').style.transform = scale(1.5);
+        document.getElementById('wallBtn').style.transform = 'scale(1.5)';
         document.getElementById('wallBtn').disabled = false;
         document.getElementById('startBtn').disabled = true;
         document.getElementById('endBtn').disabled = true;
     } 
     if(!isWall){
-        document.getElementById('wallBtn').style.transform = scale(1);
+        document.getElementById('wallBtn').style.transform = 'scale(1)';
         document.getElementById('wallBtn').disabled = false;
         document.getElementById('startBtn').disabled = false;
         document.getElementById('endBtn').disabled = false;
@@ -17,13 +17,13 @@ function enableWall(){
 function enableStart(){
     isStart = !isStart;
     if(isStart){
-        document.getElementById('startBtn').style.transform = scale(1.5);
+        document.getElementById('startBtn').style.transform = 'scale(1.5)';
         document.getElementById('wallBtn').disabled = true;
         document.getElementById('startBtn').disabled = false;
         document.getElementById('endBtn').disabled = true;
     }
     if(!isStart){
-        document.getElementById('startBtn').style.transform = scale(1);
+        document.getElementById('startBtn').style.transform = 'scale(1)';
         document.getElementById('wallBtn').disabled = false;
         document.getElementById('startBtn').disabled = false;
         document.getElementById('endBtn').disabled = false;
@@ -33,13 +33,13 @@ function enableStart(){
 function enableEnd(){
     isEnd = !isEnd;
     if(isEnd){
-        document.getElementById('endBtn').style.transform = scale(1.5);
+        document.getElementById('endBtn').style.transform = 'scale(1.5)';
         document.getElementById('wallBtn').disabled = true;
         document.getElementById('startBtn').disabled = true;
         document.getElementById('endBtn').disabled = false;
     }
     if(!isEnd){
-        document.getElementById('startBtn').style.transform = scale(1);
+        document.getElementById('endBtn').style.transform = 'scale(1)';
         document.getElementById('wallBtn').disabled = false;
         document.getElementById('startBtn').disabled = false;
         document.getElementById('endBtn').disabled = false;
